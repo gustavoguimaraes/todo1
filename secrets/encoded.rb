5b8b3bc6433841b9a69e8f0ab960cb5e1af1bce4
Gustavo <gustavoguimaraes@gmail.com>
2013-11-14 16:29:00 -0500

    Fix comments

--- secrets/encoded.rb
+++ secrets/encoded.rb
@@ -4,6 +4,8 @@ secret = "Uzo and Gustavo peed in bed until the age of 12"
 #printing it results: 1 fo ega eht litnu deb ni deep ovatsuG dna ozU
 reverse_secret = secret.reverse
 
+#This method substitute the white spaces with the last char of the secrets string reversed
+#prints 1UfoUegaUehtUlitnuUdebUniUdeepUovatsuGUdnaUozU
 gsub_reverse_secret = reverse_secret.gsub(" ", reverse_secret[-1])
 
 puts gsub_reverse_secret
