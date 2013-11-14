66d330c1e1a78d87d79130152265dd5e6ad2fb69
Gustavo <gustavoguimaraes@gmail.com>
2013-11-14 09:28:39 -0500

    Add todo3 + birthday programm

---
+++ todo3/birthday.rb
@@ -0,0 +1,11 @@
+
+date = "Nov 14th"
+
+puts "When is your birthday? For example my is Sep 19th"
+bday = gets.chomp
+
+if bday == date
+	puts "HAPPY BIRTHDAY!"
+else
+	puts "Thanks, I have that now recorded"
+end
