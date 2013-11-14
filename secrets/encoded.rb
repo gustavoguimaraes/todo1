c0cdf40ddeb7001c569b9c230ea0dbc78892141e
Gustavo <gustavoguimaraes@gmail.com>
2013-11-14 16:25:46 -0500

    Add folder called secret  and file encoder

---
+++ secrets/encoded.rb
@@ -0,0 +1,9 @@
+secret = "Uzo and Gustavo peed in bed until the age of 12"
+
+# It reverses the secret 
+#printing it results: 1 fo ega eht litnu deb ni deep ovatsuG dna ozU
+reverse_secret = secret.reverse
+
+gsub_reverse_secret = reverse_secret.gsub(" ", reverse_secret[-1])
+
+puts gsub_reverse_secret
