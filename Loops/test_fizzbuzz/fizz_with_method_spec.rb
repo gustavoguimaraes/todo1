afa8ee9a50f58c2417e1d5174415eb3c596a1ea9
Gustavo <gustavoguimaraes@gmail.com>
2013-11-18 15:23:33 -0500

    Add test for fizzbuzz

---
+++ Loops/test_fizzbuzz/fizz_with_method_spec.rb
@@ -0,0 +1,21 @@
+require './fizzbuzz'
+
+describe "#fizzbuzz" do
+
+	it "should return fizz buzz when number mod 15 = 0" do
+		expect(fizzbuzz(75)).to eq("fizzbuzz")
+	end
+
+	it "should return  buzz when 10 mod 5 == 0" do
+		expect(fizzbuzz(10)).to eq("buzz")
+	end
+
+	it "should return fizz when 9 mod 3 == 0" do
+		expect(fizzbuzz(9)).to eq("fizz")
+	end
+
+	it "should return the integer itself" do 
+		expect(fizzbuzz(13)).to eq(13)
+	end
+
+end
