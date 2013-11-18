479cef5a3b8837d2f60b1864f94049e0a34a9863
Gustavo <gustavoguimaraes@gmail.com>
2013-11-18 16:19:52 -0500

    Update file

--- Loops/test_fizzbuzz/fizz_with_method_spec.rb
+++ Loops/test_fizzbuzz/fizz_with_method_spec.rb
@@ -1,4 +1,4 @@
-require './fizzbuzz'
+require './fizzbuzzls'
 
 describe "#fizzbuzz" do
 
---
+++ tests_practice/dog_years_spec.rb
@@ -0,0 +1,21 @@
+require './dog_years'
+
+    describe "#age_in_years" do
+        it "should return input age" do
+            expect(age_in_years(75)).to eq(75)
+        end
+    end
+
+    describe "#age_in_days" do
+        it "should return age * 365" do
+            expect(age_in_days(10)).to eq(3650)
+        end
+    end
+
+    describe "#age_in_dog_years" do
+        it "should return age * 7" do
+            expect(age_in_dog_years(3)).to eq(21)
+        end
+    end
+
+end
