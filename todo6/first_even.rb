72dd4d921aa81f6d7e0523f5fbb9345b6155d17c
Gustavo <gustavoguimaraes@gmail.com>
2013-11-19 14:27:54 -0500

    Add todo6

--- todo6/first_even.rb
+++ todo6/first_even.rb
@@ -1,3 +1,12 @@
 def first_even(items)
-  #code goes here
+  even_only_array = []
+  items.each do |elem|
+  	if elem % 2 == 0
+  		even_only_array << elem
+  	end
+	end
+  even_only_array[0]
 end
+
+
+first_even([300, 40, 21, 23, 5])
