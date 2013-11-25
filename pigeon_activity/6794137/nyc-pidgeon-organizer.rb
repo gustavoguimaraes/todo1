a7c8f20a4dea2736126f83e7d138df6e2c2ea0ef
Gustavo <gustavoguimaraes@gmail.com>
2013-11-24 23:26:20 -0500

    Add somewhat original solution to pigeon problem

--- pigeon_activity/6794137/nyc-pidgeon-organizer.rb
+++ pigeon_activity/6794137/nyc-pidgeon-organizer.rb
@@ -1,8 +1,4 @@
-########################
-# NYC PIGEON ORGANIZER #
-########################
-
-# Start with the following collected data on NYC pigeons.
+require 'awesome_print'
 
 pigeon_data = {
   :color => {
@@ -25,28 +21,31 @@ pigeon_data = {
 
 pigeon_list = {}
 
-#.each and .each_pair are the same.
-pigeon_data.each_pair do |key, value|
-
-  value.each_pair do | attributes, name_inside|
-      name_inside.each do |name|
-        #go through the names and put them into a hash that but doesn't override it if the hash already exists there 
-        pigeon_list[name] = {} unless pigeon_list[name]
-        pigeon_list[name][:color] = [] unless pigeon_list[name][:color]
-        if key == :color
-          pigeon_list[name][key] << attributes
-        else
-          pigeon_list[name][key] =attributes
-        end
+pigeon_data.each do |key, value|
+    value.each do |attri, pigeon_name|
+      pigeon_name.each do |pigeon|
+      unless pigeon_list[pigeon]
+        pigeon_list[pigeon] = {} 
+      end
+       unless pigeon_list[pigeon][:color]
+        pigeon_list[pigeon][:color] = [] 
+      end
+    
+    if key == :color
+      pigeon_list[pigeon][key] << attri
+    else 
+      pigeon_list[pigeon][key] = attri
+   end
       end
     end
-end
-puts pigeon_list
+  end
+  ap pigeon_list
+
 
 # Iterate over the hash above collecting each pigeon by name and insert it
 # as the key of a new hash where each name holds the attributes for that bird. 
 # Your output should match the hash below:
-
+ 
 # pigeon_list = {
 #   "Theo" => {
 #     :color => ["purple", "grey"],
@@ -84,4 +83,3 @@ puts pigeon_list
 #     :lives => "Central Park"
 #   }
 # }
-
