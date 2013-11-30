a4eded6e7d63837a5504501ce849136311cef123
Gustavo <gustavoguimaraes@gmail.com>
2013-11-30 15:41:37 -0500

    Collects optional progress

--- Collections/optional_collections.rb
+++ Collections/optional_collections.rb
@@ -7,6 +7,7 @@ require 'awesome_print'
  
 # These problems should get progressively more difficult.
  
+#1.
 # Return true if every element of the tools array starts with an "r" and false otherwise.
   tools = ["ruby", "rspec", "rails"]
  
@@ -16,10 +17,9 @@ require 'awesome_print'
  	end
  end
 
-ap start_with_r(tools)
-#ap tools
+#ap start_with_r(tools)
 
-
+#2.
 # Create a new array from the captain_planet array with all the elements that contain the letter "a".  
   captain_planet = ["earth", "fire", "wind", "water", "heart"]
 def contain_letter_a(arr)
@@ -31,9 +31,18 @@ def contain_letter_a(arr)
 	end
 	ap a_array
 end
+#contain_letter_a(captain_planet)
 
-	#contain_letter_a(captain_planet)
+#3.
+#captain_planet refactored with the help of Chris
+def check_word_for_a(array)
+  array.select { |element| element.include?("a") }
+end
 
+#p check_word_for_a(captain_planet)
+
+
+#4.
 # Identify the first element in the stuff array that begins with the letters "wa".
   stuff = ["candy", :pepper, "wall", :ball, "wacky"]
 def wa_indentify(array)
@@ -50,6 +59,16 @@ ap wa_arr[0]
 
 #wa_indentify(stuff)
 
+#refactored
+def find_word_wa(array)
+  array.find { |element| element[0..1] == "wa" }
+end
+
+find_word_wa(stuff)
+
+
+
+#5.
 # Identify all the elements in the stuff array that begins with the letters "wa".
   stuff = ["candy", :pepper, "wall", :ball, "wacky"]
  def wa_all_indentify(array)
@@ -64,6 +83,17 @@ ap wa_arr
 
  #wa_all_indentify(stuff)
 
+#refactored
+def find_all_word_wa(array)
+  array.select { |element| element[0..1] == "wa" }
+end
+
+find_all_word_wa(stuff)
+
+
+
+
+#6.
 # Remove anything that's not a string from an array.
 # Hint: Use the method "class"  "blake".class
 
@@ -78,57 +108,125 @@ def remove_non_strings(array)
  end
 
  remove_non_strings(stuff)
- 
+
+
+ #refactored code
+def remove_non_strings(array)
+  array.delete_if do |element|
+  element.class != String
+	end
+end
+
+#remove_non_strings(stuff)
+
+#7. 
 # Change the third letter of all strings in an array.  Your solution should work for arrays that have mixed types of objects inside it.
  
+def change_char(array, char)
+	arr = []
+	array.each do |element|
+		if element.class == String
+			element[2] = char
+		end
+		arr << element
+	end
+ap arr
+end
+
+#refactored code
 def change_char(array, char)
 	array.collect do |element|
 		if element.class == String
 			element[2] = char
 		end
 	end
+ap array
 end
 
-change_char(stuff,"$")
+#change_char(stuff,"$")
 
-
+#8.
 # Count the number of times each word appears in a string and store that data in a hash that has the word 
 #as the key and the count as the value.
  
-#   string = "the flatiron school is better than general assembly"
+string = "the flatiron school is better than general assembly the the general tara tara"
+
+def count_words(str)
+	hash_words = {}
+	str.split(" ").each do |elem| 
+		if hash_words[elem].nil?
+			hash_words[elem] = 1 
+		else 
+			 hash_words[elem] +=1 
+		end
+	end
+ ap hash_words
+end
+
+#count_words(string)
  
-# Count the number of times each hash appears in the array, remove any duplicates and add a :count key to each hash with the number of times it appears.
-#   [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
-#     becomes
-#   [{:name => "blake", :count => 2}, {:name => "ashley", :count => 1}]
+#9.
+# Count the number of times each hash appears in the array, remove any duplicates and 
+#add a :count key to each hash with the number of times it appears.
+
+array1 = [{:name => "blake"}, {:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
+# becomes [{:name => "blake", :count => 3}, {:name => "ashley", :count => 1}]
+
+def hash_name_count(array)
+ 	array2 = []
+ 	array3 =[]	
+ 	num = 0
+	# hash is {:name => "blake"}
+	array.each do |hash|
+		array.each do |other_hash|
+			if hash[:name] == other_hash[:name]
+				 hash[:count] = 1
+			else
+				hash[:count] = num += 1
+			end
+
+	end
+
+		array2 << hash
+	
+	end
+	#hardcoded
+	ap array2[-1] 
+	ap array2[-2]
+end
+
+hash_name_count(array1)
+   
+
  
+ #10.
 # Take two arrays of hashes and merge the first names and last names into a new array of hashes where each hash has all information about itself.
  
-# [
-#        {
-#         :first_name => "blake"
-#     },
-#        {
-#         :first_name => "ashley"
-#     }
-# ]
-# and
-# [
-#        {
-#          "blake" => {
-#             :awesomeness => 10,
-#                  :height => "74",
-#               :last_name => "johnson"
-#         },
-#         "ashley" => {
-#             :awesomeness => 9,
-#                  :height => 60,
-#               :last_name => "dubs"
-#         }
-#     }
-# ]
+first_array = [
+       {
+        :first_name => "blake"
+    },
+       {
+        :first_name => "ashley"
+    }
+]
+ #and
+second_array = [
+       {
+         "blake" => {
+            :awesomeness => 10,
+                 :height => "74",
+              :last_name => "johnson"
+        },
+        "ashley" => {
+            :awesomeness => 9,
+                 :height => 60,
+              :last_name => "dubs"
+        }
+    }
+]
  
-# becomes
+ #becomes
 # [
 #        {
 #          :first_name => "blake",
@@ -144,43 +242,113 @@ change_char(stuff,"$")
 #     }
 # ]
  
+
+def becomes_third(first, second)
+	third = []
+	#{"blake" => {:awesomeness => 10,:height => "74",:last_name => "johnson"}
+	second.each do | array_hash_elem |
+		# iterating through each key value pair of hash_elem: key = "blake"  value = :awesomeness => 10,:height => "74",:last_name => "johnson"
+		array_hash_elem.each do | name_key, attributes  |
+			#iterating over first array hash element= {:first_name => "blake"}
+			first.each do |first_hash_element|
+				#iterating over key= :first_name,  value= "blake"
+				first_hash_element.each do |first_name_key, name_value|
+					if name_value == name_key
+						 attributes[first_name_key] = name_value
+						 third << attributes
+					end
+				end
+
+			end
+
+		end
+	
+
+	end
+ ap third
+end
+
+#becomes_third(first_array, second_array)
+
+ 
 # Return all hashes that have a value of "cool" for the :temperature key.
-# [
-#         {
-#                :name => "ashley",
-#         :temperature => "sort of cool"
-#     },
-#         {
-#                :name => "blake",
-#         :temperature => "cool"
-#     }
-# ]
+cool_array = [
+        {
+               :name => "ashley",
+        :temperature => "sort of cool"
+    },
+        {
+               :name => "blake",
+        :temperature => "cool"
+    }
+]
+
+def return_cool(array)
+	cool = []
+	#iterate over {:name => "ashley",:temperature => "sort of cool"}
+	array.each do |hash_elem|
+		#iterate over key value pair. key = :name => "ashley" value => "ashley""
+		hash_elem.each_pair do | key_name, value_temp |
+			if value_temp == "cool"
+				cool << hash_elem
+			end
+		end
+	end
+ap cool
+end
+
+#return_cool(cool_array)
  
 # Convert the nested data structure from it's current structure
- 
-# {
-#   "flatiron school bk" => {
-#     :location => "NYC",
-#     :price => "free"
-#   },
-#   "dev boot camp" => {
-#     :location => "SF"
-#     :price => "a million dollars"
-#   },
-#   "dev boot camp chicago" => {
-#     :location => "Chicago",
-#     :price => "half a million dollars"
-#   },
-#   "general assembly" => {
-#     :location => "NYC",
-#     :price => "too much"
-#   },
-#   "some school in SF" => {
-#     :location => "SF",
-#     :price => "your soul"
-#   }
-# }
 # to a structure that organizes the schools by location.
+
+current_struc = {
+  "flatiron school bk" => {
+    :location => "NYC",
+    :price => "free"
+  },
+  "dev boot camp" => {
+    :location => "SF",
+    :price => "a million dollars"
+  },
+  "dev boot camp chicago" => {
+    :location => "Chicago",
+    :price => "half a million dollars"
+  },
+  "general assembly" => {
+    :location => "NYC",
+    :price => "too much"
+  },
+  "some school in SF" => {
+    :location => "SF",
+    :price => "your soul"
+  }
+}
+
+
+
+def reorganize_struc(hash)
+new_hash= {}
+	#iterate over each hash element: {(key)"flatiron school bk" => (value){:location => "NYC",:price => "free"}
+	hash.each do |hash_elem, value_attributes|
+		if new_hash[value_attributes[:location]].nil?
+			new_hash[value_attributes[:location]] = []
+			new_hash[value_attributes[:location]]<< {:school => hash_elem, :price => value_attributes[:price]} 
+		
+		else
+			new_hash[value_attributes[:location]]<< {:school => hash_elem, :price => value_attributes[:price]} 
+		end	
+	end
+ap new_hash
+
+end
+#reorganize_struc(current_struc)
+
+
+
+
+
+
 
 
 
