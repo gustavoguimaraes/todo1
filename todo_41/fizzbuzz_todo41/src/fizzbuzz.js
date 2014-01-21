8c58026dd1b221d5b9bc288653c75ccbfd8bfb81
Gustavo <gustavoguimaraes@gmail.com>
2014-01-21 13:47:50 -0500

    Refactor code

--- todo_41/fizzbuzz_todo41/src/fizzbuzz.js
+++ todo_41/fizzbuzz_todo41/src/fizzbuzz.js
@@ -1,14 +1,18 @@
-function fizzBuzz(n) {
-  if(n % 15 == 0) {
-    return "fizzbuzz";
-  } else if(n% 3 == 0) {
-    return "fizz";
-  } else if(n % 5 == 0) {
-    return "buzz";
-  } else {
-    return n;
-  }
-}
+// function fizzBuzz(n) {
+//   if(n % 15 == 0) {
+//     return "fizzbuzz";
+//   } else if(n% 3 == 0) {
+//     return "fizz";
+//   } else if(n % 5 == 0) {
+//     return "buzz";
+//   } else {
+//     return n;
+//   }
+// }
+
+//refactored
+function fizzBuzz(n) { (n % 3 == 0)? (n % 5 == 0)?console.log("FizzBuzz"): console.log('Fizz') : (n % 5 == 0)? console.log('Buzz'): console.log(n) }
+
 
 for(var i = 1; i < 101; i++) {
    console.log(fizzBuzz(i));
