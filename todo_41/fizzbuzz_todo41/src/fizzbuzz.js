b84fdf946206b8f8823619e201cf722d01fb0e5f
Gustavo <gustavoguimaraes@gmail.com>
2014-01-22 08:09:57 -0500

    Another todo 41 Refactor

--- todo_41/fizzbuzz_todo41/src/fizzbuzz.js
+++ todo_41/fizzbuzz_todo41/src/fizzbuzz.js
@@ -12,6 +12,11 @@
 
 //refactored
 function fizzBuzz(n) { (n % 3 == 0)? (n % 5 == 0)?console.log("FizzBuzz"): console.log('Fizz') : (n % 5 == 0)? console.log('Buzz'): console.log(n) }
+ 
+//or 
+// function fizzBuzz(num){
+// (num % 15 == 0) ? console.log("fizzbuzz"):(num % 5 == 0) ? console.log("buzz") : (num % 3 == 0) ? console.log("fizz") : console.log(num)
+// }
 
 
 for(var i = 1; i < 101; i++) {
